ef6b2d95e8cee9492a7085e29511efd37d48a6a0
Kristyn Little <kristynlittle@frontier.com>
2021-02-05 05:20:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(string)
     "NO, NOT SINCE 1938!"
   elsif string.upcase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  else string != string.upcase
+  else
     "HUH?! SPEAK UP SONNY!"
   end
 end
