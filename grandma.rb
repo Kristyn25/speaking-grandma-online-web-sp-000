ea25b0875e978c4181a5c997f8bebca015e814b0
Kristyn Little <kristynlittle@frontier.com>
2021-02-05 04:19:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string != string.upcase
-    puts "HUH?! SPEAK UP SONNY!"
-  elsif string = string.upcase
+  if string = string.upcase
     puts "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
