8b344d5bb1bf0d0bc7a10e2d906741cb43224832
Kristyn Little <kristynlittle@frontier.com>
2021-02-05 03:14:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma
-  if phrase == "I LOVE YOU GRANDMA!"
+  if "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP SONNY!"
